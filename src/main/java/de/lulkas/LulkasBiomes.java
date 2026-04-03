f388e09d3fd7a902ea5fba60295ee030f198599a
Lulkas <lukas.hilger@icloud.com>
2026-04-03 20:19:35 +0200

    Initial commit

---
+++ src/main/java/de/lulkas/LulkasBiomes.java
@@ -0,0 +1,16 @@
+package de.lulkas;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LulkasBiomes implements ModInitializer {
+	public static final String MOD_ID = "lulkas_biomes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Initialized " + MOD_ID);
+	}
+}
---
+++ src/main/java/de/lulkas/LulkasBiomesDataGenerator.java
@@ -0,0 +1,11 @@
+package de.lulkas;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LulkasBiomesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
